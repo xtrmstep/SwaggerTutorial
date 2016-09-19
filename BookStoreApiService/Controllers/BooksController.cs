858a4adc2ffce9f1d125604b4d2516d9c91b8274
Alexander Goida <alexander.goida@gmail.com>
2016-09-19 11:13:59 +0300

    Added actions to API

---
+++ BookStoreApiService/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+
+namespace BookStoreApiService.Controllers
+{
+    public class BooksController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+
+        public IHttpActionResult Post()
+        {
+            return Ok();
+        }
+
+        public IHttpActionResult Put()
+        {
+            return Ok();
+        }
+
+        public IHttpActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
