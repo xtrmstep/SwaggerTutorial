bd20fef7e37d73c4123f94d724cd460a8224a8f0
Alexander Goida <alexander.goida@gmail.com>
2016-09-29 11:39:39 +0300

    Added a static database unfinished code

---
+++ BookStoreApiService/Data/Database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookStoreApiService.Models;
+
+namespace BookStoreApiService.Data
+{
+    public static class Database<T> where T: Entity, new() 
+    {
+        public static void Create(T entity)
+        {
+            
+        }
+        public static T Read(int id)
+        {
+
+        }
+
+        public static IList<T> Read()
+        {
+
+        }
+
+        public void Update(T entity)
+        {
+            
+        }
+
+        public void Delete(T entity)
+        {
+            
+        }
+    }
+}
