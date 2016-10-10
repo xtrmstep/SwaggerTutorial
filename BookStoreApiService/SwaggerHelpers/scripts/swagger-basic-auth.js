89347185cec610dd3d15711ae92cf18e51b8eed8
Alexander Goida <alexander.goida@gmail.com>
2016-10-11 02:19:32 +0300

    Added JS to automatically add Authorization header for Basic auth

---
+++ BookStoreApiService/SwaggerHelpers/scripts/swagger-basic-auth.js
@@ -0,0 +1,3 @@
+﻿// BookStoreApiService.SwaggerHelpers.scripts.swagger-basic-auth.js
+
+swaggerUi.api.clientAuthorizations.add("basic", new SwaggerClient.ApiKeyAuthorization("Authorization", "Basic U3dhZ2dlcjpUZXN0", "header"));
